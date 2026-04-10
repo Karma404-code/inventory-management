68c559a84726e6cdd78cde0cf4b29387623ec415
Karma404 <taumadhi@gmail.com>
2026-04-10 22:24:37 +0545

    product services

---
+++ src/main/java/org/example/product/ProductService.java
@@ -0,0 +1,32 @@
+package org.example.product;
+
+import java.util.List;
+
+public class ProductService {
+    public static ProductService productService;
+    ProductRepository productRepository;
+
+    private ProductService() {
+        productRepository = new ProductRepository();
+    }
+
+    public static ProductService getInstance() {
+        if (productService == null) {
+            productService = new ProductService();
+        }
+        return productService;
+    }
+
+    public void addProduct(Product product) {
+        productRepository.addProduct(product);
+    }
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
+
+    public Product getProductById(long id) {
+        return productRepository.findById(id)
+                .orElseThrow();
+    }
+}
