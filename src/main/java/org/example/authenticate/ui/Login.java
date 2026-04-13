a10ba59f57befcc7ebe541d8b7cbd0a9c6bf6fd5
Karma404 <taumadhi@gmail.com>
2026-04-13 16:00:32 +0545

    fixed layout in login page

--- src/main/java/org/example/authenticate/ui/Login.java
+++ src/main/java/org/example/authenticate/ui/Login.java
@@ -1,9 +1,11 @@
 package org.example.authenticate.ui;
 
+import net.miginfocom.swing.MigLayout;
 import org.example.authenticate.AccountService;
 import org.example.product.ui.ProductUI;
 
 import javax.swing.*;
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 
 public class Login extends JPanel {
@@ -11,6 +13,7 @@ public class Login extends JPanel {
     AccountService accountService;
 
     JFrame parentFrame;
+    JPanel loginForm;
     JTextField name;
     JPasswordField pwd;
     JLabel nameLabel, pwdLabel, createAccountLabel;
@@ -20,13 +23,22 @@ public class Login extends JPanel {
         accountService = AccountService.getInstance();
         this.parentFrame = parentFrame;
 
+        loginForm = new JPanel();
+        loginForm.setLayout(new MigLayout("insets 10"));
+
         nameLabel = new JLabel("Name:");
-        name = new JTextField();
+        name = new JTextField(15);
 
         pwdLabel = new JLabel("Password:");
-        pwd = new JPasswordField();
+        pwd = new JPasswordField(15);
 
         loginBtn = new JButton("login");
+        loginBtn.setBackground(Color.BLUE);
+        loginBtn.setForeground(Color.WHITE);
+        loginBtn.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 26));
+
+        createAccountLabel = new JLabel("Create Account:");
+
         loginBtn.addActionListener((e) ->
         {
             String inputName = name.getText();
@@ -44,13 +56,15 @@ public class Login extends JPanel {
 
         });
 
-        add(nameLabel);
-        add(name);
+        loginForm.add(nameLabel);
+        loginForm.add(name, "wrap");
 
-        add(pwdLabel);
-        add(pwd);
+        loginForm.add(pwdLabel);
+        loginForm.add(pwd,  "wrap");
 
-        add(loginBtn);
+        loginForm.add(loginBtn, "wrap, cell 1 4, growx");
+        // loginForm.add(createAccountLabel, "growx");
+        add(loginForm, BorderLayout.CENTER);
     }
 
     private void switchToProduct() {
